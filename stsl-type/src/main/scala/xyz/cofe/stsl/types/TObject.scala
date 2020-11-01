a64d867f6b7613394178e343773bd84e6c81e070
Kamnev Georgiy <nt.gocha@gmail.com>
2020-11-02 04:10:00 +0500

    check args

--- stsl-type/src/main/scala/xyz/cofe/stsl/types/TObject.scala
+++ stsl-type/src/main/scala/xyz/cofe/stsl/types/TObject.scala
@@ -1,5 +1,16 @@
 package xyz.cofe.stsl.types
 
-class TObject( Name:String ) extends Obj {
+class TObject( Name:String,
+               ogenerics:GenericParams=GenericParams(),
+               ofields:Fields=Fields(),
+               omethods:Methods=Methods()
+             ) extends Obj {
+  require(Name!=null)
+  require(ogenerics!=null)
+  require(ofields!=null)
+  require(omethods!=null)
   override val name: String = Name
+  override lazy val generics: GenericParams = ogenerics
+  override lazy val fields: Fields = ofields
+  override lazy val methods: Methods = omethods
 }
