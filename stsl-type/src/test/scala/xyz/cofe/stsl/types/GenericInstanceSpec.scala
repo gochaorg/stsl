9a7898ba81675675e7cebf35965f0113a5f47c7f
Kamnev Georgiy <nt.gocha@gmail.com>
2022-06-27 02:51:26 +0500

    to spec

--- stsl-type/src/test/scala/xyz/cofe/stsl/types/GenericInstanceSpec.scala
+++ stsl-type/src/test/scala/xyz/cofe/stsl/types/GenericInstanceSpec.scala
@@ -3,8 +3,9 @@ package xyz.cofe.stsl.types
 import org.junit.jupiter.api.Test
 import Type._
 import JvmType._
+import org.scalatest.flatspec.AnyFlatSpec
 
-class GenericInstanceTest {
+class GenericInstanceSpec extends AnyFlatSpec {
   val listType = TObject("List")
     .generics(AnyVariant("A"))
     .fields("size" -> INT)
@@ -30,27 +31,26 @@ class GenericInstanceTest {
     .fields("name" -> INT)
     .build
 
-  @Test
-  def test01():Unit = {
+  "Generic экземпляр List" should "?" in {
     println("test01()")
     println("===============")
-
+  
     val gi1 = new GenericInstance(
       Map("A" -> userType),
       listType
     )
-
+  
     println(gi1)
-
+  
     val gi2 = new GenericInstance(
       Map("A" -> TypeVariable("B", THIS)),
       listType
     )
-
+  
     println(gi2)
-
+  
     val gi3 = gi2.typeVarReplace("B" -> TypeVariable("C",THIS))
-
+  
     println(gi3)
   }
 }
