b39020004b729521c72cba641afa33a7303195cc
Kamnev Georgiy <nt.gocha@gmail.com>
2022-07-22 21:52:14 +0500

    comments

--- stsl-parser/src/main/scala/xyz/cofe/stsl/tast/AnonymousObject.scala
+++ stsl-parser/src/main/scala/xyz/cofe/stsl/tast/AnonymousObject.scala
@@ -2,11 +2,22 @@ package xyz.cofe.stsl.tast
 
 import xyz.cofe.stsl.types.TAnon
 
+/**
+ * Работа с TAnon
+ */
 object AnonymousObject {
+  /**
+   * Ключ в Map
+   */
   val TypeDefinition: Object = new Object {
     override def toString: String = "TypeDefinition"
   }
   
+  /**
+   * Получает тип для Map
+   * @param anonObj экземпляр Map
+   * @return тип
+   */
   def definitionOf( anonObj:java.util.Map[Any,Any] ):Option[TAnon] = {
     require(anonObj!=null)
     anonObj.get(TypeDefinition) match {
